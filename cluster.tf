f30af64cd71cc620288033ca74646617086d02bf
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2020-10-04 06:20:44 +0200

    Update: refactoring Kapsule variables
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- cluster.tf
+++ cluster.tf
@@ -15,7 +15,7 @@
 resource "scaleway_k8s_cluster_beta" "main" {
   name              = var.name
   region            = var.region
-  description       = var.description
+  description       = format("%s. Created by Terraform", var.description)
   version           = var.k8s_version
   cni               = var.cni
   enable_dashboard  = var.enable_dashboard
@@ -24,21 +24,29 @@ resource "scaleway_k8s_cluster_beta" "main" {
   feature_gates     = var.feature_gates
   admission_plugins = var.admission_plugins
 
-  autoscaler_config {
-    disable_scale_down              = var.disable_scale_down
-    scale_down_delay_after_add      = var.scale_down_delay_after_add
-    scale_down_unneeded_time        = var.scale_down_unneeded_time
-    estimator                       = var.estimator
-    expander                        = var.expander
-    ignore_daemonsets_utilization   = var.ignore_daemonsets_utilization
-    balance_similar_node_groups     = var.balance_similar_node_groups
-    expendable_pods_priority_cutoff = var.expendable_pods_priority_cutoff
+  dynamic "autoscaler_config" {
+    for_each = var.enable_cluster_autoscaler ? [1] : []
+
+    content {
+      disable_scale_down              = var.disable_scale_down
+      scale_down_delay_after_add      = var.scale_down_delay_after_add
+      scale_down_unneeded_time        = var.scale_down_unneeded_time
+      estimator                       = var.estimator
+      expander                        = var.expander
+      ignore_daemonsets_utilization   = var.ignore_daemonsets_utilization
+      balance_similar_node_groups     = var.balance_similar_node_groups
+      expendable_pods_priority_cutoff = var.expendable_pods_priority_cutoff
+    }
   }
 
-  auto_upgrade {
-    enable                        = var.enable_auto_upgrade
-    maintenance_window_start_hour = var.maintenance_window_start_hour
-    maintenance_window_day        = var.maintenance_window_day
+  dynamic "auto_upgrade" {
+    for_each = var.enable_auto_upgrade ? [1] : []
+
+    content {
+      enable                        = var.enable_auto_upgrade
+      maintenance_window_start_hour = var.maintenance_window_start_hour
+      maintenance_window_day        = var.maintenance_window_day
+    }
   }
 
 }
--- variables.tf
+++ variables.tf
@@ -15,12 +15,21 @@
 #######################################################################
 # Provider
 
+variable region {
+  default     = null
+  description = "The region in which the cluster should be created."
+}
 
 #######################################################################
 # Kubernetes cluster
 
 variable name {
   description = "The name of the cluster"
+  type        = string
+}
+
+variable description {
+  description = "Description of the cluster"
   type        = string
 }
 
@@ -50,6 +59,12 @@ variable ingress {
   }
 }
 
+variable enable_dashboard {
+  default     = false
+  type        = bool
+  description = "(Optional) Enables the Kubernetes dashboard."
+}
+
 variable tags {
   type        = list(string)
   default     = []
@@ -58,6 +73,7 @@ variable tags {
 
 variable feature_gates {
   default     = []
+  type        = list(string)
   description = "The list of feature gates to enable on the cluster."
 }
 
@@ -66,52 +82,83 @@ variable admission_plugins {
   description = "The list of admission plugins to enable on the cluster."
 }
 
-variable region {
+
+variable enable_cluster_autoscaler {
+  default     = false
+  type        = bool
+  description = "(Optional) Enables the Kubernetes cluster autoscaler."
+}
+
+variable disable_scale_down {
+  default     = false
+  type        = bool
+  description = "(Defaults to false) Disables the scale down feature of the autoscaler."
+}
+
+variable scale_down_delay_after_add {
+  default     = "10m"
+  type        = string
+  description = "(Defaults to 10m) How long after scale up that scale down evaluation resumes."
+}
+
+variable scale_down_unneeded_time {
+  default     = "10m"
+  type        = string
+  description = "(Default to 10m) How long a node should be unneeded before it is eligible for scale down."
+}
+
+variable estimator {
+  default     = "binpacking"
+  type        = string
+  description = "(Defaults to binpacking) Type of resource estimator to be used in scale up."
+}
+
+variable expander {
+  default     = "random"
+  type        = string
+  description = "(Default to random) Type of node group expander to be used in scale up."
+}
+
+variable ignore_daemonsets_utilization {
+  default     = false
+  type        = bool
+  description = "(Defaults to false) Ignore DaemonSet pods when calculating resource utilization for scaling down."
+}
+
+variable balance_similar_node_groups {
+  default     = false
+  type        = bool
+  description = "(Defaults to false) Detect similar node groups and balance the number of nodes between them."
+}
+
+variable expendable_pods_priority_cutoff {
+  default     = "-10"
+  type        = string
+  description = "(Defaults to -10) Pods with priority below cutoff will be expendable. They can be killed without any consideration during scale down and they don't cause scale up. Pods with null priority (PodPriority disabled) are non expendable."
+}
+
+variable enable_auto_upgrade {
+  default     = false
+  type        = bool
+  description = "(Optional) Set to true to enable Kubernetes patch version auto upgrades. ~> Important: When enabling auto upgrades, the version field take a minor version like x.y (ie 1.18)."
+}
+
+variable maintenance_window_start_hour {
   default     = null
-  description = "(Defaults to provider region) The region in which the cluster should be created."
+  type        = string
+  description = "(Optional) The start hour (UTC) of the 2-hour auto upgrade maintenance window (0 to 23). Required if enable_auto_upgrade is true"
 }
 
-variable autoscaler_config {
-  default = null
-  type = object({
-    enabled                         = bool
-    disable_scale_down              = bool
-    scale_down_delay_after_add      = string
-    scale_down_unneeded_time        = string
-    estimator                       = string
-    expander                        = string
-    ignore_daemonsets_utilization   = bool
-    balance_similar_node_groups     = bool
-    expendable_pods_priority_cutoff = number
-  })
-  description = "The configuration options for the Kubernetes cluster autoscaler."
-}
-
-variable auto_upgrade {
-  default = null
-  type = object({
-    enable                        = bool
-    maintenance_window_start_hour = number
-    maintenance_window_day        = string
-  })
-  description = "The auto upgrade configuration."
+variable maintenance_window_day {
+  default     = null
+  type        = string
+  description = "(Optional) The day of the auto upgrade maintenance window (monday to sunday, or any). Required if enable_auto_upgrade is true"
 }
 
 #######################################################################
 # Node pools
 
-variable "node_pools" {
-  type = list
-  default = [{
-    node_type          = "GP1-XS"
-    size               = 1
-    min_size           = 1
-    max_size           = 3
-    autohealing        = true
-    autoscaling        = false
-    placement_group_id = null
-    container_runtime  = "docker"
-    tags               = []
-  }]
+variable node_pools {
+  type        = map
   description = "Node pools configuration for Kubernetes cluster."
 }
